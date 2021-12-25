5cb3374421b540ada0af828f136b39c63af9c6fc
bret99 <64787351+bret99@users.noreply.github.com>
2021-12-25 13:16:01 +0400

    Update keys_to_renew.sh

--- keys_to_renew.sh
+++ keys_to_renew.sh
@@ -1,13 +1,17 @@
 for item in [ $(ls /etc/openvpn/easy-rsa/keys/ | grep ovpn) ];do
 echo -e "\033[1;90mSearching match for \033[1;37m$item\033[1;00m"
 # cert expired in 10 days current year next month
-if [[ $(grep After /etc/openvpn/easy-rsa/keys/$item | awk '{print $7}') = $(date +%Y) && $(grep After /etc/openvpn/easy-rsa/keys/$item | awk '{print $4}') = $(date -d '+10 days' | awk '{print $2}') && $(($(da$echo $item >> /tmp/keys_to_renew.txt
+if [[ $(grep After /etc/openvpn/easy-rsa/keys/$item | awk '{print $7}') = $(date +%Y) && $(grep After /etc/openvpn/easy-rsa/keys/$item | awk '{print $4}') = $(date -d '+10 days' | awk '{print $2}') && $(($(da$
+echo $item >> /tmp/keys_to_renew.txt
+echo -e "Match found for \033[1;92m$item\033[1;00m"
 fi
 # cert expired in 10 days next year
 if [[ $(grep After /etc/openvpn/easy-rsa/keys/$item | awk '{print $7}') > $(($(date +%Y))) && $(grep After /etc/openvpn/easy-rsa/keys/$item | awk '{print $4}') = 'Jan' && $(($(date -d '+10 days' +%d))) = $(gr$echo $item >> /tmp/keys_to_renew.txt
+echo -e "Match found for \033[1;92m$item\033[1;00m"
 fi
 # cert expired in 10 days current month
 if [[ $(grep After /etc/openvpn/easy-rsa/keys/$item | awk '{print $7}') = $(date +%Y) && $(grep After /etc/openvpn/easy-rsa/keys/$item | awk '{print $4}') = $(date +%b) && $(($(date -d '+10 days' +%d))) = $(g$echo $item >> /tmp/keys_to_renew.txt
+echo -e "Match found for \033[1;92m$item\033[1;00m"
 fi
 done
 
